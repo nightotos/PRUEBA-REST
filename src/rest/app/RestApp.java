b8b95b53ad633d2536e1ea557ad58f9d52be522b
nightotos <nightotos@gmail.com>
2019-01-15 13:35:43 -0500

    json parse
    Signed-off-by: nightotos <nightotos@gmail.com>

--- src/rest/app/RestApp.java
+++ src/rest/app/RestApp.java
@@ -1,21 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rest.app;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Connection;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-/**
- *
- * @author PC
- */
 public class RestApp {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void main(String[] args) throws MalformedURLException, IOException {
+       
+     URL url = new URL("https://jsonplaceholder.typicode.com/users"); 
+     HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+     conexion.setRequestMethod("GET");
+     conexion.setRequestProperty("Accept", "application/json");
+     
+     InputStreamReader dat = new InputStreamReader(conexion.getInputStream());
+     BufferedReader buffer = new BufferedReader(dat);
+     String salida = null;
+     String datos_global;
+     datos_global = "";
+      
+     while ((salida = buffer.readLine()) != null) {
+               
+        datos_global+=salida;
+                         
+            }
+    
+     JSONArray datos_array=new JSONArray(datos_global);
+     
+     
+    Connection miConexion;
+        
+        String ruta="jdbc:mysql://localhost:3306/java";
+        String user="java";
+        String password="java"; 
+     
+    for(int y=0;y<datos_array.length();y++){
+    JSONObject jsonobject=datos_array.getJSONObject(y);
+    System.out.println(jsonobject.get("website"));
+    }
+     
+     
+     conexion.disconnect();   
     }
     
 }
